d01033c5f909008bb524ab29bc79c521e4781dea
Karlemami <florian.philippegm@gmail.com>
2024-04-28 19:45:17 +0200

    feat : ajout decision tree et random forest

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,6 +1,8 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_predict
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def naive_bayes(matrix, classes):
@@ -12,4 +14,14 @@ def naive_bayes(matrix, classes):
 def svm(matrix, classes):
     svm_classifier = SVC(kernel='linear')
     y_pred = cross_val_predict(svm_classifier, matrix, classes, cv=10)
+    return y_pred
+
+def decision_tree(matrix, classes):
+    dt_classifier = DecisionTreeClassifier()
+    y_pred = cross_val_predict(dt_classifier, matrix, classes, cv=10)
+    return y_pred
+
+def random_forest(matrix, classes):
+    rf_classifier = RandomForestClassifier()
+    y_pred = cross_val_predict(rf_classifier, matrix, classes, cv=10)
     return y_pred
